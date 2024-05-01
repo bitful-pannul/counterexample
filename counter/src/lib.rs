940c80c0916176f9efb58095b42b2f5b33888110
bitful-pannul <bitful-pancreas@protonmail.com>
2024-05-01 12:53:43 -0400

    read initial count

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -1,9 +1,26 @@
-use kinode_process_lib::{await_message, call_init, println, Address, Response};
+use kinode_process_lib::{await_message, call_init, eth::{Address as EthAddress, Provider, TransactionInput, TransactionRequest, U256}, println, Address, Response};
+use alloy_sol_types::{sol, SolCall, SolValue};
+use std::str::FromStr;
 
 wit_bindgen::generate!({
     path: "wit",
     world: "process",
 });
+
+sol! {
+    contract Counter {
+        uint256 public number;
+    
+        function setNumber(uint256 newNumber) public {
+            number = newNumber;
+        }
+    
+        function increment() public {
+            number++;
+        }
+    }
+}
+
 
 fn handle_message(_our: &Address) -> anyhow::Result<()> {
     let message = await_message()?;
@@ -24,6 +41,26 @@ fn handle_message(_our: &Address) -> anyhow::Result<()> {
 call_init!(init);
 fn init(our: Address) {
     println!("begin");
+
+    let counter_address = EthAddress::from_str("0x1234567890123456789012345678901234567890").unwrap();
+
+    let provider = Provider::new(31337, 5);
+    let count = Counter::numberCall {}.abi_encode();
+
+    let tx = TransactionRequest::default()
+        .to(Some(counter_address))
+        .input(TransactionInput::new(count.into()));
+    let x = provider.call(tx, None);
+
+    match x {
+        Ok(b) => {
+            let number = U256::abi_decode(&b, false).unwrap();
+            println!("initial count: {:?}", number);
+        }
+        Err(e) => {
+            println!("error getting initial count: {:?}", e);
+        }
+    }
 
     loop {
         match handle_message(&our) {
