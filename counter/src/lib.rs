6d591a21760408e5431417b143666118fa34c669
bitful-pannul <bitful-pancreas@protonmail.com>
2024-05-01 12:29:37 -0400

    init

---
+++ counter/src/lib.rs
@@ -0,0 +1,36 @@
+use kinode_process_lib::{await_message, call_init, println, Address, Response};
+
+wit_bindgen::generate!({
+    path: "wit",
+    world: "process",
+});
+
+fn handle_message(_our: &Address) -> anyhow::Result<()> {
+    let message = await_message()?;
+
+    if !message.is_request() {
+        return Err(anyhow::anyhow!("unexpected Response: {:?}", message));
+    }
+
+    let body: serde_json::Value = serde_json::from_slice(message.body())?;
+    println!("got {body:?}");
+    Response::new()
+        .body(serde_json::to_vec(&serde_json::json!("Ack")).unwrap())
+        .send()
+        .unwrap();
+    Ok(())
+}
+
+call_init!(init);
+fn init(our: Address) {
+    println!("begin");
+
+    loop {
+        match handle_message(&our) {
+            Ok(()) => {}
+            Err(e) => {
+                println!("error: {:?}", e);
+            }
+        };
+    }
+}
