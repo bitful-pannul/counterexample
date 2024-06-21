a46965f94d1baecbaab23a217ab67feb6d811268
bitful-pannul <bitful-pancreas@protonmail.com>
2024-06-21 15:45:35 -0700

    update to 0.8

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -1,21 +1,24 @@
-use alloy_consensus::{SignableTransaction, TxEnvelope, TxLegacy};
-use alloy_network::TxSignerSync;
-use alloy_primitives::TxKind;
+use alloy::{
+    consensus::{SignableTransaction, TxEip1559, TxEnvelope},
+    network::{eip2718::Encodable2718, TxSignerSync},
+    primitives::TxKind,
+    rpc::types::eth::TransactionRequest,
+    signers::local::PrivateKeySigner,
+};
 use alloy_rlp::Encodable;
-use alloy_rpc_types::TransactionRequest;
-use alloy_signer_wallet::LocalWallet;
 use alloy_sol_types::{sol, SolCall, SolValue};
 use kinode_process_lib::{
     await_message, call_init,
-    eth::{Address as EthAddress, Provider, TransactionInput, U256},
+    eth::{Address as EthAddress, Provider, U256},
     println, Address, Response,
 };
+
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
 wit_bindgen::generate!({
-    path: "wit",
-    world: "process",
+    path: "target/wit",
+    world: "process-v0",
 });
 
 sol! {
@@ -32,7 +35,7 @@ sol! {
     }
 }
 
-pub const COUNTER_ADDRESS: &str = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+pub const COUNTER_ADDRESS: &str = "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82";
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum CounterAction {
@@ -41,7 +44,11 @@ pub enum CounterAction {
     SetNumber(u64),
 }
 
-fn handle_message(_our: &Address, provider: &Provider, wallet: &LocalWallet) -> anyhow::Result<()> {
+fn handle_message(
+    _our: &Address,
+    provider: &Provider,
+    wallet: &PrivateKeySigner,
+) -> anyhow::Result<()> {
     let message = await_message()?;
 
     if !message.is_request() {
@@ -61,21 +68,29 @@ fn handle_message(_our: &Address, provider: &Provider, wallet: &LocalWallet) -> 
                 .get_transaction_count(wallet.address(), None)
                 .unwrap()
                 .to::<u64>();
-            let mut tx = TxLegacy {
-                chain_id: Some(31337),
+
+            let gas = provider
+                .get_gas_price()
+                .map_err(|e| anyhow::anyhow!("couldn't get gas price: {:?}", e))?;
+
+            // todo: use TransactionRequest once signature encoding works.
+            let mut tx = TxEip1559 {
+                chain_id: 31337,
                 nonce: nonce,
                 to: TxKind::Call(EthAddress::from_str(COUNTER_ADDRESS).unwrap()),
-                gas_limit: 100000,
-                gas_price: 100000000,
+                gas_limit: 15000000,
+                max_fee_per_gas: 10000000000,
+                max_priority_fee_per_gas: 300000000,
                 input: increment.into(),
                 ..Default::default()
             };
-
+            // let gas = provider.estimate_gas(tx.clone().into(), None);
             let sig = wallet.sign_transaction_sync(&mut tx)?;
+
             let signed = TxEnvelope::from(tx.into_signed(sig));
 
             let mut buf = vec![];
-            signed.encode(&mut buf);
+            signed.encode_2718(&mut buf);
 
             let tx_hash = provider.send_raw_transaction(buf.into());
             println!("tx_hash: {:?}", tx_hash);
@@ -90,16 +105,17 @@ fn handle_message(_our: &Address, provider: &Provider, wallet: &LocalWallet) -> 
                 .get_transaction_count(wallet.address(), None)
                 .unwrap()
                 .to::<u64>();
-            let mut tx = TxLegacy {
-                chain_id: Some(31337),
+
+            let mut tx = TxEip1559 {
+                chain_id: 31337,
                 nonce: nonce,
                 to: TxKind::Call(EthAddress::from_str(COUNTER_ADDRESS).unwrap()),
-                gas_limit: 100000,
-                gas_price: 100000000,
+                gas_limit: 15000000,
+                max_fee_per_gas: 10000000000,
+                max_priority_fee_per_gas: 300000000,
                 input: set_number.into(),
                 ..Default::default()
             };
-
             let sig = wallet.sign_transaction_sync(&mut tx)?;
             let signed = TxEnvelope::from(tx.into_signed(sig));
 
@@ -124,12 +140,13 @@ fn read(provider: &Provider) -> anyhow::Result<U256> {
 
     let tx = TransactionRequest::default()
         .to(counter_address)
-        .input(TransactionInput::new(count.into()));
+        .input(count.into());
+
     let x = provider.call(tx, None);
 
     match x {
         Ok(b) => {
-            let number = U256::abi_decode(&b, false).unwrap();
+            let number = U256::abi_decode(&b, false)?;
             println!("current count: {:?}", number.to::<u64>());
             Ok(number)
         }
@@ -148,9 +165,13 @@ fn init(our: Address) {
 
     let _count = read(&provider);
 
-    let wallet =
-        LocalWallet::from_str("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
-            .unwrap();
+    let wallet = PrivateKeySigner::from_str(
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+    )
+    .unwrap();
+
+    // EthereumWallet doesn't have proper traits yet.
+    // let wallet: EthereumWallet = wallet.into();
 
     loop {
         match handle_message(&our, &provider, &wallet) {
